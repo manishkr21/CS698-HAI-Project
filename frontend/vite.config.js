8bbd2f50996704e013e763e60290c4c0fb1cee4f
manish kumar <manishk21@iitk.ac.in>
2025-11-05 22:31:06 +0530

    vercel v1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:8001',
+        target: process.env.VITE_API_URL || 'http://localhost:8001/',
         changeOrigin: true,
         secure: false,
       }
