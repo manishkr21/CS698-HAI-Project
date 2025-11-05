91a2667bb9e7ccd5d95063b1b43071ef09fe95ac
manish kumar <manishk21@iitk.ac.in>
2025-11-05 23:12:53 +0530

    update for vercel v3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,46 +1,20 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: process.env.VITE_BASE_PATH || '/CS698-HAI-Project',
-//   server: {
-//     proxy: {
-//       '/api': 'http://localhost:8001'
-//     },
-//     fs: {
-//       strict: true
-//     },
-//     // headers: {
-//     //   'Content-Type': 'application/javascript',
-//     //   '.jsx': 'application/javascript',
-//     //   '.js': 'application/javascript',
-//     //   '.ts': 'application/javascript',
-//     //   '.tsx': 'application/javascript'
-//     // }
-//   },
-//   build: {
-//     outDir: 'dist'
-//   }
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/CS698-HAI-Project',
+  base: process.env.VITE_BASE_PATH || '/CS698-HAI-Project/',
   server: {
     proxy: {
-      '/api': {
-        target: process.env.VITE_API_URL || 'http://localhost:8001/',
+      '/student_app': {
+        target: 'https://cdis.iitk.ac.in',
         changeOrigin: true,
-        secure: false,
+        secure: true
       }
-    },
+    }
   },
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    sourcemap: true
   }
 })
